efc13bcf49ab5a1a1679839aa2221fbd299b5db5
Ritesh Bhirud <riteshbhirud@gmail.com>
2026-01-17 02:47:53 -0500

    Initial Commit

---
+++ ib_clone/ib_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ib_cloneApp.swift
+//  ib_clone
+//
+//  Created by rb on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct ib_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
